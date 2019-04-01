64de2e9288cd0f3671bb34137a4f2be09f50aa2a
Artem Manchenkov <artyom@manchenkoff.me>
2019-04-01 22:24:53 +0300

    lesson 01 update

---
+++ lesson-01/debug.py
@@ -0,0 +1,6 @@
+a = 10
+a = a + 10
+b = a * 2
+c = "john"
+
+print()
---
+++ lesson-01/terminal.py
@@ -0,0 +1,8 @@
+a = 10
+a = a + 10
+b = a * 2
+
+name = "John"
+
+# print(name + 10) # ошибка типов
+print(name * 10)  # ошибки нет
